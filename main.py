758bc7ad440aab2630f073091856ba94e16832c2
FedorKhokhryakov <92886745+FedorKhokhryakov@users.noreply.github.com>
2022-01-23 22:41:52 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import threading
 import random
 
+from animated_sprite import AnimatedSprite
 from enemy_tank import EnemyTank
 from board import Board
 from create_level import tile_width, tile_height, tile_images
@@ -67,6 +68,34 @@ class Shot(pygame.sprite.Sprite):
             self.kill()
 
 
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(explosions)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+def destruction():
+    boom = AnimatedSprite(load_image("boom.png"), 5, 2, 5, 5)
+    pass
+
+
 def shoot(shots1, pos1, tank_shoots):
     if pos1 == "up" and not tank_shoots.is_collide_up or \
             pos1 == "down" and not tank_shoots.is_collide_down or \
@@ -110,6 +139,7 @@ if __name__ == '__main__':
     all_sprites = pygame.sprite.Group()
     shots = pygame.sprite.Group()
     tanks = pygame.sprite.Group()
+    explosions = pygame.sprite.Group()
     running = True
 
     MYEVENTTYPE = pygame.USEREVENT + 1
