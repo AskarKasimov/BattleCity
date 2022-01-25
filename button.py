c6db025a5eb2c47e065e721f58f1754049b27c1d
AskarTheBest <59764983+AskarKasimov@users.noreply.github.com>
2022-01-25 17:42:36 +0300

    готово к защите

--- button.py
+++ button.py
@@ -1,8 +1,10 @@
 import pygame
 
 
+# класс кнопки
 class Button:
-    def __init__(self, surface, x, y, name, activated=False, color=(230, 230, 230)):
+    def __init__(self, surface, x, y, name, activated=False, color=(230, 230, 230), isactive=True):
+        self.active = isactive
         self.surface = surface
         self.x = x
         self.y = y
@@ -22,7 +24,7 @@ class Button:
             text_w = text.get_width()
             text_h = text.get_height()
             rect = pygame.draw.rect(self.surface, (130, 130, 130), (text_x - 7, text_y - 7,
-                                                                text_w + 16, text_h + 12), 0)
+                                                                    text_w + 16, text_h + 12), 0)
             self.checkbox_obj = pygame.Rect(rect.x, rect.y, rect.width, rect.height)
             self.surface.blit(text, (text_x, text_y))
         elif self.hold:
@@ -33,7 +35,7 @@ class Button:
             text_w = text.get_width()
             text_h = text.get_height()
             pygame.draw.rect(self.surface, (190, 190, 190), (text_x - 7, text_y - 7,
-                                                         text_w + 16, text_h + 12), 0)
+                                                             text_w + 16, text_h + 12), 0)
             self.surface.blit(text, (text_x, text_y))
         else:
             font = pygame.font.Font(None, 24)
@@ -43,17 +45,17 @@ class Button:
             text_w = text.get_width()
             text_h = text.get_height()
             rect = pygame.draw.rect(self.surface, self.color, (text_x - 7, text_y - 7,
-                                                           text_w + 16, text_h + 12), 0)
+                                                               text_w + 16, text_h + 12), 0)
             self.checkbox_obj = pygame.Rect(rect.x, rect.y, rect.width, rect.height)
             self.surface.blit(text, (text_x, text_y))
 
     def update(self, event_object, others=None):
-        if event_object.type == pygame.MOUSEBUTTONUP:
+        if event_object.type == pygame.MOUSEBUTTONUP and self.active:
             x, y = event_object.pos
             px, py, w, h = self.checkbox_obj
             if px < x < px + w and py < y < py + h:
                 self.checked = not self.checked
-        if event_object.type == pygame.MOUSEMOTION:
+        if event_object.type == pygame.MOUSEMOTION and self.active:
             x, y = event_object.pos
             px, py, w, h = self.checkbox_obj
             if px < x < px + w and py < y < py + h:
